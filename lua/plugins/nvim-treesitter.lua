586fe4ac86855dda4213534f23f93556f47420fa
Pujan Khunt <pujankhunt2412@gmail.com>
2025-07-12 00:09:05 +0530

    copy pasted the nvim-treesitter config from previous config

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  lazy = false,
+  opts = {
+    ensure_installed = {
+      "javascript",
+      "typescript",
+      "c",
+      "cpp",
+      "java",
+      "lua",
+      "python",
+      "bash"
+    },
+    highlight = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+    },
+    indent = {
+      enable = false,
+    }
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
