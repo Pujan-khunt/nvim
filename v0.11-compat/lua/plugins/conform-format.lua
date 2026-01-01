230949fff3bf96ef5f8f3a13079495276ce94c9e
Pujan Khunt <pujankhunt2412@gmail.com>
2026-01-01 18:11:54 +0530

    Remove format notifications and add active formatters in status line

--- v0.11-compat/lua/plugins/conform-format.lua
+++ v0.11-compat/lua/plugins/conform-format.lua
@@ -9,29 +9,11 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 		},
-		format_after_save = function(bufnr)
-			--- @type conform.FormatOpts
-			local formatOptions = {
-				async = true,
-				lsp_format = "fallback",
-				stop_after_first = true,
-			}
-
-			local conform = require("conform")
-			local formatters = conform.list_formatters_to_run(bufnr)
-
-			-- Collect the names of all formatters
-			local fmt_names = {}
-			for _, fmt in ipairs(formatters) do
-				table.insert(fmt_names, fmt.name)
-			end
-
-			if #fmt_names > 0 then
-				local msg = "Formatting with: " .. table.concat(fmt_names, ", ")
-				vim.notify(msg, vim.log.levels.INFO, { timeout = 3000, title = "Conform" })
-			end
-
-			return formatOptions
-		end,
+		--- @type conform.FormatOpts
+		format_after_save = {
+			async = true,
+			lsp_format = "fallback",
+			stop_after_first = true,
+		},
 	},
 }
--- v0.11-compat/lua/plugins/lualine.lua
+++ v0.11-compat/lua/plugins/lualine.lua
@@ -3,5 +3,36 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+
+	--- @module "lualine"
+	opts = {
+		globalstatus = true,
+		sections = {
+			lualine_x = { "lsp_status" },
+			lualine_y = {
+				function()
+					local conform = require("conform")
+					local status, formatters = pcall(conform.list_formatters_to_run, 0)
+					if not status then
+						return ""
+					end
+
+					local fmt_names = {}
+					for _, formatter in ipairs(formatters) do
+						table.insert(fmt_names, formatter.name)
+					end
+
+					if #fmt_names > 0 then
+						return "󰷈 " .. table.concat(fmt_names, ", ")
+					end
+					return ""
+				end,
+			},
+			lualine_z = { "progress" },
+		},
+		tabline = {
+			lualine_x = { "filename" },
+			lualine_y = { "filesize" },
+		},
+	},
 }
